e8c6a2ab516856304a280e16eb4f56cc48365639
lmontan <148903756+lmontan@users.noreply.github.com>
2025-04-13 14:23:24 -0400

    just added some of the scripts
    I Added some scripts for game

---
+++ Assets/Scripts/TriggerAnimations.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAnimations : MonoBehaviour
+{
+    public string enterAnimationTrigger = "PlayAnimation"; // Animation to play on trigger enter
+    public string exitAnimationTrigger = "StopAnimation";  // Animation to play on trigger exit
+    public Animator animator; // Reference to the Animator component
+
+    private void Start()
+    {
+        if (animator == null)
+        {
+            animator = GetComponent<Animator>(); // Get the Animator if not assigned
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (animator != null)
+        {
+            animator.SetTrigger(enterAnimationTrigger);
+        }
+        else
+        {
+            Debug.LogWarning("Animator component is missing on " + gameObject.name);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (animator != null)
+        {
+            animator.SetTrigger(exitAnimationTrigger);
+        }
+        else
+        {
+            Debug.LogWarning("Animator component is missing on " + gameObject.name);
+        }
+    }
+}
